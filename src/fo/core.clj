cb741706667e278edd1bdf46643f352114148c21
ahxxm <i@ahxxm.com>
2018-10-24 11:38:28 +0800

    fix auto-reload code: start-(reloadable)-server

--- src/fo/core.clj
+++ src/fo/core.clj
@@ -4,9 +4,10 @@
   (:require
    [taoensso.timbre :refer [refer-timbre]]
    [clojure.java.io :as jio]
-   [compojure.core :as compojure :refer [GET]]
+   [compojure.core :as compojure :refer [GET defroutes]]
    [compojure.route :as route]
    [ring.middleware.params :as params]
+   [ring.middleware.reload :refer [wrap-reload]]
    [aleph.http :as http])
   (:gen-class))
 
@@ -24,15 +25,13 @@
     (debug "no such page" req)
     rsp))
 
-(def handler
-  (params/wrap-params
-   (compojure/routes
-    (GET "/" [] home)
-    (route/not-found not-found))))
+(defroutes app
+  (GET "/" [] home)
+  (route/not-found not-found))
 
 (defn -main
   "I don't do a whole lot."
   []
   ;; TODO: read port from env
   (info "start http server at 1000")
-  (http/start-server handler {:port 10000}))
+  (http/start-server (wrap-reload #'app) {:port 10000}))
