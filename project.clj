ff5fcef9cb8518f613b886bf2e6519e20489fd8a
ahxxm <i@ahxxm.com>
2018-10-24 10:48:45 +0800

    hello world

--- project.clj
+++ project.clj
@@ -3,4 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/core.async "0.4.474"]
+
+                 [compojure "1.6.1"]
+                 [ring "1.7.0"]
+                 [aleph "0.4.6"]]
+
+  :main fo.core
+  :aot [fo.core]
+  )
--- src/fo/core.clj
+++ src/fo/core.clj
@@ -1,6 +1,29 @@
-(ns fo.core)
+(ns fo.core
+  (:import
+    [io.netty.handler.ssl SslContextBuilder])
+  (:require
+    [compojure.core :as compojure :refer [GET]]
+    [ring.middleware.params :as params]
+    [compojure.route :as route]
+    [aleph.http :as http]
+    [clojure.core.async :as a]
+    [clojure.java.io :refer [file]])
+  (:gen-class))
 
-(defn foo
+
+(defn hello-world-handler
+  [req]
+  {:status 200
+   :headers {"content-type" "text/plain"}
+   :body "hello world!"})
+
+(def handler
+  (params/wrap-params
+    (compojure/routes
+      (GET "/hello"         [] hello-world-handler)
+      (route/not-found "No such page."))))
+
+(defn -main
   "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+  []
+  (http/start-server handler {:port 10000}))
