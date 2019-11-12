4a81af5c379bc087a1b988ddf13434bf8d001294
ahxxm <i@ahxxm.com>
2019-11-12 14:23:52 +0800

    bump deps

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject fo "0.1.0-SNAPSHOT"
   :description "与佛论禅"
-  :url "https://fo.lolis.cc"
+  :url "https://folol.herokuapp.com/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
 
                  [com.taoensso/timbre "4.10.0"]
-                 [buddy/buddy-core "1.4.0"]
+                 [buddy/buddy-core "1.6.0"]
                  [compojure "1.6.1"]
                  [ring "1.7.0"]
                  [aleph "0.4.6"]]
