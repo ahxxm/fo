e4c508c24104cc8c43d06af2ecdf314621784382
ahxxm <i@ahxxm.com>
2022-05-21 19:17:25 +0800

    bump deps

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "https://folol.herokuapp.com/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
 
-                 [com.taoensso/timbre "5.1.2"]
-                 [buddy/buddy-core "1.10.1"]
-                 [compojure "1.6.2"]
-                 [ring "1.9.3"]
-                 [aleph "0.4.6"]]
+                 [com.taoensso/timbre "5.2.1"]
+                 [buddy/buddy-core "1.10.413"]
+                 [compojure "1.6.3"]
+                 [ring "1.9.5"]
+                 [aleph "0.4.7"]]
 
   :resource-paths ["resources/"]
   :ring {:auto-refresh? true :auto-reload? true
